cde89cdc29c336883e563bd294ab26f09ebe4f4f
Reggie Thompson <reggie.thompson4@gmail.com>
2021-02-28 16:25:01 -0700

    Draft charts for displaying data

--- src/charts.js
+++ src/charts.js
@@ -1,40 +1,170 @@
 import Chart from 'chart.js';
 
-var waterChart = document.getElementById('waterChart');
-var waterConsumed = new Chart(waterChart, {
-    type: 'bar',
-    data: {
-        labels: ['Sun', 'Mon', 'Tue', 'Wed', 'Thur', 'Fri', 'Sat'],
-        datasets: [{
-            label: '# of Votes',
-            data: [12, 19, 3, 5, 2, 3],
-            backgroundColor: [
-                'rgba(255, 99, 132, 0.2)',
-                'rgba(54, 162, 235, 0.2)',
-                'rgba(255, 206, 86, 0.2)',
-                'rgba(75, 192, 192, 0.2)',
-                'rgba(153, 102, 255, 0.2)',
-                'rgba(255, 159, 64, 0.2)'
-            ],
-            borderColor: [
-                'rgba(255, 99, 132, 1)',
-                'rgba(54, 162, 235, 1)',
-                'rgba(255, 206, 86, 1)',
-                'rgba(75, 192, 192, 1)',
-                'rgba(153, 102, 255, 1)',
-                'rgba(255, 159, 64, 1)'
-            ],
-            borderWidth: 1
-        }]
-    },
-    options: {
-        scales: {
-            yAxes: [{
-                ticks: {
-                    beginAtZero: true
-                }
-            }]
+var hydrationChart = document.getElementById('hydrationChart');
+var waterConsumed = new Chart(hydrationChart, {
+  type: 'bar',
+  data: {
+    labels: ['Sun', 'Mon', 'Tue', 'Wed', 'Thur', 'Fri', 'Sat'],
+    datasets: [{
+      label: 'Ounces of water drank in the week of :',
+      data: getTestData(),
+      backgroundColor: 'rgba(54, 162, 235, 0.2)',
+      borderColor: 'rgba(54, 162, 235, 1)',
+      borderWidth: 1
+    }]
+  },
+  options: {
+    scales: {
+      yAxes: [{
+        ticks: {
+          beginAtZero: true,
+          callback: function(value) {
+            return `${value} oz.`;
+          }
         }
+      }]
     }
+  }
 });
 
+var stepCountChart = document.getElementById('stepCountChart');
+var weeklySteps = new Chart(stepCountChart, {
+  type: 'bar',
+  data: {
+    labels: ['Sun', 'Mon', 'Tue', 'Wed', 'Thur', 'Fri', 'Sat'],
+    datasets: [{
+      label: 'Steps taken this week: ',
+      data: getTestData(),
+      backgroundColor: 'rgba(54, 162, 235, 0.2)',
+      borderColor: 'rgba(54, 162, 235, 1)',
+      borderWidth: 1
+    }]
+  },
+  options: {
+    scales: {
+      yAxes: [{
+        ticks: {
+          beginAtZero: true,
+          callback: function(value) {
+            return value;
+          }
+        }
+      }]
+    }
+  }
+});
+
+var flightsOfStairsChart = document.getElementById('flightsOfStairsChart');
+var flightsWalked = new Chart(flightsOfStairsChart, {
+  type: 'bar',
+  data: {
+    labels: ['Sun', 'Mon', 'Tue', 'Wed', 'Thur', 'Fri', 'Sat'],
+    datasets: [{
+      label: 'Flights of stairs walked this week:',
+      data: getTestData(),
+      backgroundColor: 'rgba(54, 162, 235, 0.2)',
+      borderColor: 'rgba(54, 162, 235, 1)',
+      borderWidth: 1
+    }]
+  },
+  options: {
+    scales: {
+      yAxes: [{
+        ticks: {
+          beginAtZero: true,
+          callback: function(value) {
+            return value;
+          }
+        }
+      }]
+    }
+  }
+});
+
+var activeMinutesChart = document.getElementById('activeMinutesChart');
+var minutesActive = new Chart(activeMinutesChart, {
+  type: 'bar',
+  data: {
+    labels: ['Sun', 'Mon', 'Tue', 'Wed', 'Thur', 'Fri', 'Sat'],
+    datasets: [{
+      label: 'Minutes active this week:',
+      data: getTestData(),
+      backgroundColor: 'rgba(54, 162, 235, 0.2)',
+      borderColor: 'rgba(54, 162, 235, 1)',
+      borderWidth: 1
+    }]
+  },
+  options: {
+    scales: {
+      yAxes: [{
+        ticks: {
+          beginAtZero: true,
+          callback: function(value) {
+            return `${value} mins.`;
+          }
+        }
+      }]
+    }
+  }
+});
+
+var hoursSleptChart = document.getElementById('hoursSleptChart');
+var hoursSlept = new Chart(hoursSleptChart, {
+  type: 'bar',
+  data: {
+    labels: ['Sun', 'Mon', 'Tue', 'Wed', 'Thur', 'Fri', 'Sat'],
+    datasets: [{
+      label: 'Ounces of water drank in the week of :',
+      data: getTestData(),
+      backgroundColor: 'rgba(54, 162, 235, 0.2)',
+      borderColor: 'rgba(54, 162, 235, 1)',
+      borderWidth: 1
+    }]
+  },
+  options: {
+    scales: {
+      yAxes: [{
+        ticks: {
+          beginAtZero: true,
+          callback: function(value) {
+            return `${value} hrs.`;
+          }
+        }
+      }]
+    }
+  }
+});
+
+var increasinglyActiveChart = document.getElementById('increasinglyActiveChart');
+var activeStreak = new Chart(increasinglyActiveChart, {
+  type: 'bar',
+  data: {
+    labels: ['Sun', 'Mon', 'Tue', 'Wed', 'Thur', 'Fri', 'Sat'],
+    datasets: [{
+      label: 'Current streak of increased activity:',
+      data: getTestData(),
+      backgroundColor: 'rgba(54, 162, 235, 0.2)',
+      borderColor: 'rgba(54, 162, 235, 1)',
+      borderWidth: 1
+    }]
+  },
+  options: {
+    scales: {
+      yAxes: [{
+        ticks: {
+          beginAtZero: true,
+          callback: function(value) {
+            return value;
+          }
+        }
+      }]
+    }
+  }
+});
+
+
+function getTestData() {
+  return [10, 20, 30, 40, 50, 60, 70]
+}
+
+export default waterConsumed
